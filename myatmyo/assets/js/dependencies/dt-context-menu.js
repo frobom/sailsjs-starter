d0f93359cccf3139526389b9d81e969c3fe96edc
myatmoe <myat.moe.thwel@frobom.com>
2016-09-09 17:35:19 +0630

    update folder.js, add jquery files, navigate through table cell within same group

---
+++ myatmyo/assets/js/dependencies/dt-context-menu.js
@@ -0,0 +1,621 @@
+(function() {
+  
+  "use strict";
+
+
+  $('a[name=save]').on('click', function(event) {
+    event.stopImmediatePropagation();
+
+    createJsonObject();
+    
+  });
+
+$(document).delegate('td','click',function(event) {
+    event.stopImmediatePropagation();
+
+    $("td.active").find("input").blur();
+    $("td.active").removeClass("active");
+
+    $(this).addClass("active");
+    $(this).find("input").focus();  
+  });
+
+// focus the clicked row (no need to scroll because it's visible
+$("#table_id tbody tr").on("click", function() {
+    $("#table_id tbody tr.active").removeClass("active");
+    $(this).addClass("active");
+});
+
+  var keys = { 'ArrowLeft':37, 'ArrowUp':38, 'ArrowRight':39, 'ArrowDown':40 } 
+
+  
+    $(document).keydown(function(e) {      
+      e.stopImmediatePropagation();
+
+      var activeCell = $("td.active");
+      var cellIndex = $(activeCell).parent().children().index($(activeCell));
+
+      if (e.ctrlKey  && e.keyCode == keys.ArrowLeft) {
+
+        if ($(activeCell).prev().is("th") == false) {
+          $(activeCell).prev().find("input").focus();
+          $(activeCell).prev().addClass("active");
+          $(activeCell).blur();
+          $(activeCell).removeClass("active");          
+        }             
+      }
+      else if (e.ctrlKey  && e.keyCode == keys.ArrowRight) {
+
+        if ($(activeCell).next().length) {
+
+          $(activeCell).blur();
+          $(activeCell).removeClass("active");
+          $(activeCell).next().find("input").focus();
+          $(activeCell).next().addClass("active");
+        }
+      }
+      else if (e.ctrlKey  && e.keyCode == keys.ArrowUp) {
+
+      // if ($(activeCell).parent().prev().length == 0) {
+
+      //   if ($(activeCell).parents("tbody").prev().length) {
+
+      //     $(activeCell).parents("tbody").prev().children().eq(cellIndex).find("input").focus();
+      //     $(activeCell).parents("tbody").prev().children().eq(cellIndex).addClass("active");
+      //   }
+      // }        
+
+        if ($(activeCell).parent().prev().length) {
+          $(activeCell).blur();
+          $(activeCell).removeClass("active");
+          $(activeCell).parent().prev().children().eq(cellIndex).find("input").focus();
+          $(activeCell).parent().prev().children().eq(cellIndex).addClass("active");          
+        }
+        
+      }
+      else if (e.ctrlKey  && e.keyCode == keys.ArrowDown) {
+
+        // if ($(activeCell).parent().next().length == 0) {          
+
+        //   if ($(activeCell).parents("tbody").next().length) {
+
+
+        //     alert($(activeCell).parents("tbody").next().children().eq(cellIndex).html());
+
+        //     $(activeCell).parents("tbody").next().children().eq(cellIndex).find("input").focus();
+        //     $(activeCell).parents("tbody").next().children().eq(cellIndex).addClass("active");
+        //   }
+        // }
+
+        if ($(activeCell).parent().next().length) {
+          $(activeCell).blur();
+          $(activeCell).removeClass("active");
+          $(activeCell).parent().next().children().eq(cellIndex).find("input").focus();
+          $(activeCell).parent().next().children().eq(cellIndex).addClass("active");          
+        }
+        
+      }
+    });        
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //
+  // H E L P E R    F U N C T I O N S
+  //
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Function to check if we clicked inside an element with a particular class
+   * name.
+   * 
+   * @param {Object} e The event
+   * @param {String} className The class name to check against
+   * @return {Boolean}
+   */
+  function clickInsideElement( e, className ) {
+    var el = e.srcElement || e.target;
+    
+    if ( el.classList.contains(className) ) {
+      return el;
+    } else {
+      while ( el = el.parentNode ) {
+        if ( el.classList && el.classList.contains(className) ) {
+          return el;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Get's exact position of event.
+   * 
+   * @param {Object} e The event passed in
+   * @return {Object} Returns the x and y position
+   */
+  function getPosition(e) {
+    var posx = 0;
+    var posy = 0;
+
+    if (!e) var e = window.event;
+    
+    if (e.pageX || e.pageY) {
+      posx = e.pageX;
+      posy = e.pageY;
+    } else if (e.clientX || e.clientY) {
+      posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    return {
+      x: posx,
+      y: posy
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //
+  // C O R E    F U N C T I O N S
+  //
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  
+  /**
+   * Variables.
+   */
+  var contextMenuClassName = "context-menu";
+  var contextMenuItemClassName = "context-menu__item";
+  var contextMenuLinkClassName = "context-menu__link";
+  var contextMenuActive = "context-menu--active";
+
+  var taskItemClassName = "task";
+  var taskItemInContext;
+
+  var clickCoords;
+  var clickCoordsX;
+  var clickCoordsY;
+
+  var menu = document.querySelector("#context-menu");
+  var menuItems = menu.querySelectorAll(".context-menu__item");
+  var menuState = 0;
+  var menuWidth;
+  var menuHeight;
+  var menuPosition;
+  var menuPositionX;
+  var menuPositionY;
+
+  var windowWidth;
+  var windowHeight;
+
+  var row;
+  var currentColumn;
+  var headerText = 0;
+  var html;
+  var fileName;  
+
+  $(document).delegate('tr','contextmenu',function(event) {
+    row = $(this);
+    
+  });
+
+  $(document).delegate('td','contextmenu',function(event) {
+
+    if (event.handled !== true) {
+      currentColumn = $(this).parent().children().index($(this));
+      console.log('currentColumn: ' + currentColumn);
+
+      var text = $("#header").find('th').eq(currentColumn).text();      
+      headerText = parseInt(text) || 0;
+
+      event.handled = true;
+    }
+    
+  });  
+
+
+  /**
+   * Initialise our application's code.
+   */
+  function init() {
+    contextListener();
+    clickListener();
+    keyupListener();
+    resizeListener();     
+
+  }
+
+
+  /**
+   * Listens for contextmenu events.
+   */
+  function contextListener() {
+    document.addEventListener( "contextmenu", function(e) {
+
+      taskItemInContext = clickInsideElement( e, taskItemClassName );
+
+      if ( taskItemInContext ) {
+        e.preventDefault();
+        toggleMenuOn();
+        positionMenu(e);        
+      } else {
+        taskItemInContext = null;
+        toggleMenuOff();
+      }
+    }, true);
+  }
+  
+
+  /**
+   * Listens for click events.
+   */
+  function clickListener() {
+
+    var clickHandle = function(e) {
+
+      if (e.handled !== true) {
+        var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
+
+        if ( clickeElIsLink ) {
+          e.preventDefault();
+          menuItemListener( clickeElIsLink );
+        } else {
+          var button = e.which || e.button;
+          if ( button === 1 ) {
+            toggleMenuOff();
+          }
+        }
+        e.handled = true;
+      }      
+
+    };
+
+    document.addEventListener("click", clickHandle, true);
+
+    // document.addEventListener( "click", function(e) {
+
+    //   alert("click event");
+    //   var clickeElIsLink = clickInsideElement( e, contextMenuLinkClassName );
+
+    //   if ( clickeElIsLink ) {
+    //     e.preventDefault();
+    //     menuItemListener( clickeElIsLink );
+    //   } else {
+    //     var button = e.which || e.button;
+    //     if ( button === 1 ) {
+    //       toggleMenuOff();
+    //     }
+    //   }
+    // });
+  }
+
+  /**
+   * Listens for keyup events.
+   */
+  function keyupListener() {
+    window.onkeyup = function(e) {
+      if ( e.keyCode === 27 ) {
+        toggleMenuOff();
+      }
+    }
+  }
+
+  /**
+   * Window resize event listener
+   */
+  function resizeListener() {
+    window.onresize = function(e) {
+      toggleMenuOff();
+    };
+  }
+
+  /**
+   * Turns the custom context menu on.
+   */
+  function toggleMenuOn() {
+    if ( menuState !== 1 ) {
+      menuState = 1;
+      menu.classList.add( contextMenuActive );
+    }
+  }
+
+  /**
+   * Turns the custom context menu off.
+   */
+  function toggleMenuOff() {
+    if ( menuState !== 0 ) {
+      menuState = 0;
+      menu.classList.remove( contextMenuActive );
+    }
+  }
+
+  /**
+   * Positions the menu properly.
+   * 
+   * @param {Object} e The event
+   */
+  function positionMenu(e) {
+    clickCoords = getPosition(e);
+    clickCoordsX = clickCoords.x;
+    clickCoordsY = clickCoords.y;
+
+    menuWidth = menu.offsetWidth + 4;
+    menuHeight = menu.offsetHeight + 4;
+
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    if ( (windowWidth - clickCoordsX) < menuWidth ) {
+      menu.style.left = windowWidth - menuWidth + "px";
+    } else {
+      menu.style.left = clickCoordsX + "px";
+    }
+
+    if ( (windowHeight - clickCoordsY) < menuHeight ) {
+      menu.style.top = windowHeight - menuHeight + "px";
+    } else {
+      menu.style.top = clickCoordsY + "px";
+    }
+  }
+
+  /**
+   * Dummy action function that logs an action when a menu item link is clicked
+   * 
+   * @param {HTMLElement} link The link that was clicked
+   */
+  function menuItemListener( link ) {
+    
+    console.log( "Task ID - " + taskItemInContext.getAttribute("data-id") + ", Task action - " + link.getAttribute("data-action"));
+    console.log("currentColumn " + currentColumn);
+
+    toggleMenuOff();
+    
+    if (link.getAttribute("data-action").includes("Row")) {
+      headerText = row.find("th").text();
+      console.log("headerText : " + headerText);
+    }
+    if (link.getAttribute("data-action") == "Add Row Above") {
+      addNewRow("above", row);
+    }
+    else if (link.getAttribute("data-action") == "Add Row Below") {
+      addNewRow("below", row);
+    }
+    else if (link.getAttribute("data-action") == "Add Column Left") {
+      addColumn(currentColumn, 'before', headerText);
+    }
+    else if (link.getAttribute("data-action") == "Add Column Right") {
+      addColumn(currentColumn, 'after', headerText);
+    }
+    else if (link.getAttribute("data-action") == "Delete Row") {
+      if (row.index() != -1) {
+        updateRowHeaders();
+      }
+      row.remove();      
+    }
+     else if (link.getAttribute("data-action") == "Delete Column") {
+
+      if (currentColumn != $("table tr td:last-child").index()) {
+        
+        var h = $("table th").eq(currentColumn).next();
+
+        while(h.index() != -1) {
+          h.text(parseInt(h.text())-1);
+          h = h.next();
+        }
+
+      }
+
+      $("table").find('tr').each(function(e, row) {
+        //alert("row : " + $(row).html());
+        $(row).find('th, td').eq(currentColumn).remove();
+      });
+
+    }     
+  }
+
+  function updateRowHeaders() {
+    var conditionRowsCount = $( "#condition tr" ).length;
+    
+      var r = row.next();
+      while (r.index() != -1) {
+        r.find('th').text(parseInt(r.find('th').text())-1);
+        r = r.next();
+      }   
+
+  }
+
+  function addNewRow( place, selectedRow ) {
+    
+    var nextRowIndex = 0;
+    var r = null;    
+
+    if (place == "above") {
+
+      prepareHtmlToInsertRow(headerText);            
+
+      $(html).insertBefore(selectedRow);
+
+      selectedRow.find("th").text(++headerText);
+
+      r = selectedRow.next();        
+          
+     }
+
+   else {
+
+      prepareHtmlToInsertRow(++headerText);      
+
+      $(html).insertAfter(selectedRow);
+
+      r = selectedRow.next().next();           
+    }
+
+   nextRowIndex = r.index();
+
+   while (nextRowIndex != -1) {
+      r.find("th").text(++headerText);
+      r = r.next();
+      nextRowIndex = r.index();
+    }
+
+    console.log("html : " + html);
+
+    console.log("==================");
+  }
+
+  function prepareHtmlToInsertRow( headerText ) {    
+    var table = document.getElementById("dtTable");
+
+    html = "<tr class='task'> <th>" + headerText + "</th>";
+
+    for (var i = 1; i < table.rows[0].cells.length; i++) {
+      html += "<td class='task'> <input type='text'> </td>";      
+    }
+
+    html += "</tr>";
+  }
+
+ 
+function addColumn(currentColumn,afterOrBefore,headerText) {
+
+  var insertedHeader;
+  var colIndex = currentColumn - 1;
+
+  if (afterOrBefore=='before') {
+    insertedHeader = addNewColumnHeader(afterOrBefore, currentColumn, headerText++, insertedHeader);
+  }
+  else {
+    insertedHeader = addNewColumnHeader(afterOrBefore, currentColumn, ++headerText, insertedHeader);
+  }   
+
+  if (insertedHeader.next().index() != -1) {
+    updateOtherColumnHeader(insertedHeader, headerText);   
+  }
+
+  addCells(colIndex, afterOrBefore);
+}
+
+function addNewColumnHeader(afterOrBefore, currentColumn, headerText, insertedHeader) {
+  var header = $('table tr:first th');
+  
+  if (afterOrBefore=='before') {
+    
+    $('<th class="task">' + headerText +'</th>').insertBefore($(header[currentColumn]));
+    $(header[currentColumn]).text(++headerText);
+    return $(header[currentColumn]);      
+  }
+  else {
+
+    $('<th class="task">' + headerText +'</th>').insertAfter($(header[currentColumn]));
+    return $(header[currentColumn]).next();
+  }
+}
+
+function updateOtherColumnHeader(insertedHeader, headerText) {
+  var nextColHeader = insertedHeader.next();     
+
+  while (nextColHeader.index() != -1) {
+    nextColHeader.text(++headerText);
+    nextColHeader = nextColHeader.next();
+  }
+}
+
+function addCells(colIndex, afterOrBefore) {
+
+  var allRows=$('table').find('tr');
+    $.each(allRows,function(index,value){
+          //alert("value : " + value);
+          var cells = $(value).find('td');
+          if(afterOrBefore=='before')
+            {
+              $('<td><input type="text"></td>').insertBefore($(cells[colIndex]));
+            }
+           else
+             {
+                $('<td><input type="text"></td>').insertAfter($(cells[colIndex]));
+              }
+              //$(element).on('click', function () { add_img(); });
+    });
+}
+
+function createJsonObject() {
+
+var json = {
+  conditions : [],
+  actions : [],
+  rules :[]  
+}; 
+
+var table = document.getElementById("dtTable");
+var colIndex = 1;
+var conditionRowsCount = $( "#condition tr" ).length;
+
+addConditions(json, colIndex, conditionRowsCount, table);
+
+addActions(json, colIndex, conditionRowsCount+1, table);
+
+colIndex++;
+
+addRules(json, colIndex, conditionRowsCount, table);
+
+var jsonObject = new Object();
+jsonObject.dt = json;
+  
+console.log("jsonObject : " + JSON.stringify(jsonObject));
+
+$("#dtTable").before('<p>' + JSON.stringify(jsonObject) + '</p>');
+  
+}
+
+function addConditions(json, colIndex, conditionRowsCount, table) {
+  console.log("conditionRowsCount : " + conditionRowsCount);
+
+  for (var i = 1; i <= conditionRowsCount; i++) {
+
+    json.conditions.push(table.rows[i].cells[colIndex].children[0].value);
+   
+  }
+}
+
+function addActions(json, colIndex, startIndex, table) {
+  var totalRowsCount = $( "#dtTable tr" ).length;
+
+  for (var i = startIndex; i < totalRowsCount; i++) {
+
+      json.actions.push(table.rows[i].cells[colIndex].children[0].value);      
+  }
+}
+
+function addRules(json, colIndex, conditionRowsCount, table){
+  var colsCount = table.rows[0].cells.length;
+  console.log("colsCount : " + colsCount);
+
+  for (colIndex; colIndex < colsCount; colIndex++) {
+
+   var rule = {
+    conditions : [],
+    actions : []
+   };
+
+    for (var i = 1; i <= conditionRowsCount; i++) {
+      rule.conditions.push(table.rows[i].cells[colIndex].children[0].value);        
+    }    
+
+    for (var i = conditionRowsCount+1; i < $( "#dtTable tr" ).length; i++) {
+
+      rule.actions.push(table.rows[i].cells[colIndex].children[0].value);
+    }
+    
+      json.rules.push(rule);
+      //console.log(JSON.stringify(rule));
+  }
+}
+ 
+  /**
+   * Run the app.
+   */
+  init();
+
+})();
---
+++ myatmyo/assets/js/dependencies/main.js
@@ -0,0 +1,279 @@
+
+
+$(document).ready( function() {	
+
+	var fileId = 0;
+	var divId = 0;
+	var arrayIndex;
+	var oldDivId;
+
+	var centerDiv = $('#centerID');
+
+	var listAndDivArray = [];
+
+	// Grab the first sheet, regardless of media
+	var sheet = document.styleSheets[0];	
+
+	sheet.insertRule("#conditions { border-bottom: 3px solid blue; }", 1);
+	console.log(document.styleSheets[0]);	
+
+	$("#restoreButtonID").click(function(){
+
+		if($(this).html() == "-"){
+
+			$(this).html("+");
+
+			$("#rightID").parent().css({position: 'absolute'});
+
+			$("#rightID").css({bottom:0, right: 0,width:"5%", height:"25px",position:'absolute'});
+
+			$(".center").css({width:"86%"});
+
+			$(".bottomOfCenter").css({width:"86%"});
+
+		}
+
+		else{
+
+			$(this).html("-");
+
+			$("#rightID").parent().css({position: 'absolute'});
+
+			$("#rightID").css({right:0,height:'100%',width:'15%',position:'absolute'});
+
+			$(".center").css({width:" 71%"});	
+
+			$(".bottomOfCenter").css({width:"71%"});
+		}	
+
+	});
+
+	$('.createButton').click(function(){
+
+		var newTextBoxDiv = $('<div/>').attr("id", "textBoxID");		
+
+		newTextBoxDiv.after().html('<form action=""><input type="text" id="textbox"></form>');			
+
+		if($('#textBoxID').length == 0){
+
+			$('#treeID li').show();
+
+			$('#minusID').text('- -');
+
+
+			$('.newProjectCreate').append(newTextBoxDiv);
+
+			$('#textbox').keydown(function(event) {
+
+				var message = $("#textbox").val();
+
+				if (event.keyCode == 13) {
+
+					if (message == "") {
+
+						alert("Enter Some Text In Textarea");
+
+					} else {
+
+						$('#textBoxID').remove();
+
+						//var div =$('<div/>').appendTo('#tree').attr({"class":"listdiv"});
+
+						//var ul = $('<ul/>').appendTo('.newProjectCreateDiv').attr({"id":"tree"});	
+
+						//var spans = $('<span/>').appendTo('#treeID').attr({"class":"context-menu-one"});
+
+						var li = $('<li id="'+ (++fileId) +'" />').appendTo('#treeID').text(message);
+						console.log("fileId " + fileId);
+						divId = "dtDiv" + fileId;
+
+						console.log("divId : " + divId);
+
+						var listAndDiv = {
+								listId : "",
+								divElement : ""
+							};						
+							
+						var div = $('<div id="'+ divId +'" />').appendTo('#centerID').hide();
+						
+						div = drawDecisionTable(message , div);
+						
+
+						console.log("div " + $(div).html());
+
+						listAndDiv.listId = fileId;
+						listAndDiv.divElement = div;
+
+						listAndDivArray.push(listAndDiv);						
+												
+						$(div).hide();
+					}
+						// return false;
+				}
+
+			});
+			
+
+			$(".newProjectCreate ul").on("click", "li", function(event){
+
+				$("#centerID").empty();
+
+		    	var id = $(this).attr('id');		    	
+		    	
+		    	for (var i=0; i<listAndDivArray.length; i++) {
+
+		    		if (listAndDivArray[i].listId == id) {
+		    			//console.log("element in array " + $(listAndDivArray[i].divElement).html());
+		    			$("#centerID").prepend($(listAndDivArray[i].divElement).show());
+		    			
+		    			break;
+		    		}
+		    	}
+
+		    	$("#centerID div:not(#dtDiv" + id + ")").hide();
+		    	
+		    	console.log("listId : " + id);
+		
+			});	
+
+		}			
+
+	});
+
+	
+	$('#minusID').click(function(){
+			
+			if($('#minusID').text()=='- -'){
+
+				$('#treeID li').hide("fast", function(){
+
+					$('#minusID').text('++');
+
+				});
+
+			}else{
+
+				$('#treeID li').show("fast", function(){
+
+					$('#minusID').text('- -');
+
+				});
+
+			}
+
+		});	
+
+	
+	function drawDecisionTable(fileName, dtDiv) {		
+
+		var divId = "#" + $(dtDiv).attr('id');
+
+		$(dtDiv).append('<h2 align="center">' + fileName + '</h2>');
+		$(dtDiv).append('<table id="dtTable"></table>');
+		var table = $(divId +' table');
+		  
+		table.append('<thead id="header"> <tr> <th></th> <th class="task"></th> </tr> </thead>');
+		//$('<img id="createdImage" src="some.jpg"/>').appendTo(document.body).css(style);
+		//$('<tbody id="condition"/>').appendTo(table).css("border-bottom", "3px solid blue");
+		//$("<style/>").text("#condition { border-bottom: 3px solid blue; }").appendTo("head");
+		table.append('<tbody id="condition" class="condition" style="border: 3px solid blue;"></tbody>');
+		table.append('<tbody id="action"></tbody>');
+		//$("<style/>").text("#condition { border-bottom: 3px solid blue; }").appendTo("head");
+		//$('head').append("< style > #condition { border-bottom: 3px solid blue;}  < /style >");
+		//$("head").append("<style/>").text("#condition { border-bottom: 3px solid blue; }");
+		
+		// console.log("dtDiv " + $(dtDiv).html());
+		//console.log("table " + $(table).html());		
+
+		var response = '{"dt":{"conditions":[""],"actions":[""],"rules":[{"conditions":[],"actions":[]}]}}';
+		//var response = '{"dt":{"conditions":["c1","c2","c3"],"actions":["a1","a2", "a3"],"rules":[{"conditions":["rc1","rc2","rc3"],"actions":["ra1","ra2", "a3"]},{"conditions":["r3c1","r3c2","r3c3"],"actions":["r3a1","r3a2", "a3"]},{"conditions":["r2c1","r2c2","r2c3"],"actions":["r2a1","r2a2", "a3"]}]}}';
+		
+		//convert string to JSON
+		var dt = $.parseJSON(response);
+
+		var conditions = dt.dt.conditions;
+		var actions = dt.dt.actions;
+		var rules = dt.dt.rules;
+
+		var headerRow = $(divId +' #header tr');
+		var conditionSection = $($(table).find('tbody')[0]);
+		var actionSection = $($(table).find('tbody')[1]);
+
+		//alert("conditionSection " + $('table').find('tbody')[0].html());
+
+		$.each(conditions, function(index, condition) {
+			$(conditionSection).append('<tr class="task"> <th>' + (index+1) +'</th><td><input type="text" value="' + condition +'"></td></tr>');		
+		});
+		
+
+		$.each(actions, function(index, action) {
+			$(actionSection).append('<tr class="task"> <th>' + (index+1) +'</th><td><input type="text" value="' + action +'"></td></tr>');		
+		});
+
+		$.each(rules, function(i, rule) {	
+			$(headerRow).append('<th class="task">' + (i+1) +'</th>');
+		});
+
+		var rows = $(conditionSection).find('tr');
+
+		$.each(rows, function(j, row) {
+			$.each(rules, function(k, rule) {
+				if(typeof(rule.conditions[j])  === "undefined") {
+					rule.conditions[j] = "";
+				}
+				$(row).append('<td><input type="text" value="' + rule.conditions[j] +'"></td>');
+			});
+		});
+
+		rows = $(actionSection).find('tr');
+
+		$.each(rows, function(j, row) {
+			$.each(rules, function(k, rule) {
+				if(typeof(rule.actions[j])  === "undefined") {
+					rule.actions[j] = "";
+				}
+				$(row).append('<td><input type="text" value="' + rule.actions[j] +'"></td>');
+			});
+		});
+
+		// the first row will be active by default
+		 
+		var inputElement =$($(conditionSection).find('td')[0]).find("input");
+
+		$(inputElement).focus();		
+
+		$($(conditionSection).find('td')[0]).addClass("active");			
+
+		var lastConditionRow = $(divId + " #condition tr:last" );
+
+		console.log("last row " + $(lastConditionRow).html());
+		$(lastConditionRow).css("border-bottom", "3px solid blue");
+
+		//$("#condition").css("border", "10px solid red");
+
+		return dtDiv;
+		//$("#condition").css({ borderBottom: "2px solid #ff4141" });
+		//alert("condition : " + $("#condition").html());
+		//document.getElementById("condition").style.borderBottom = "15px solid blue";
+		// $("#condition").css("border", "10px solid red");
+		// alert("css of condition : " + $("#condition").css("border"));
+		//$("p").css("background-color", "yellow");
+
+		//$(".menu").css({ "border-bottom": "2px solid #ff4141": });
+		//$("table").css("border", "3px solid blue");
+
+		//$(conditionSection).css("borderBottom", "3px solid blue");
+		//alert($(conditionSection).html());
+		// $(this).attr('style', 'text-align: center');
+	}
+
+
+
+});
+
+$( function() {
+
+	$( "#treeID" ).selectable();	
+
+});
+
